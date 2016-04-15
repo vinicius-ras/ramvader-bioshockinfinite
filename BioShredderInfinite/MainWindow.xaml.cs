1833f5278af01cc2e42ac50a0d88d5a4047d3a80
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2016-04-15 19:54:19 +0000

    * Created the basic user interface of the trainer. Most of the interface is still not working. * Resources are now public, so they can be statically accessed and used in XAML. * Implemented the donation button's functionality.

--- BioShredderInfinite/MainWindow.xaml.cs
+++ BioShredderInfinite/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace BioShredderInfinite
 {
@@ -7,6 +8,15 @@ namespace BioShredderInfinite
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		#region PRIVATE CONSTANTS
+		/// <summary>The URL which points to the webpage that runs when the user clicks the "Donate!" button of the trainer.</summary>
+		private const string PROJECT_DONATION_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=WJ2D2KRMPRKBS&lc=US&item_name=Supporting%20Vinicius%2eRAS%27%20open%20source%20projects&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted";
+		#endregion
+
+
+
+
+
 		#region PUBLIC METHODS
 		/// <summary>
 		/// Constructor.
@@ -14,6 +24,33 @@ namespace BioShredderInfinite
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+		#endregion
+
+
+
+
+
+		#region EVENT HANDLERS
+		/// <summary>
+		/// Called when the user clicks the "Donate!" button.
+		/// </summary>
+		/// <param name="sender">Object which sent the event.</param>
+		/// <param name="e">Arguments from the event.</param>
+		private void ButtonClickDonate( object sender, RoutedEventArgs e )
+		{
+			Process.Start( PROJECT_DONATION_URL );
+		}
+
+
+		/// <summary>
+		/// Called whenever any of the CheckBoxes used to activate/deactivate cheats from the trainer gets checked or unchecked.
+		/// </summary>
+		/// <param name="sender">Object which sent the event.</param>
+		/// <param name="e">Arguments from the event.</param>
+		private void CheckBoxCheatToggled( object sender, RoutedEventArgs e )
+		{
+
 		}
 		#endregion
 	}

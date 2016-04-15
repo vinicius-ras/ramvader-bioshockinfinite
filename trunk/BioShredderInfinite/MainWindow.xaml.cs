5208b09053ac37d490ea1cc07758b597a2bd4345
viniciusras <viniciusras@c4f62d13-1d80-41e3-aef9-1809c6e54439>
2016-04-15 17:49:29 +0000

    * Created the BioShredder project. That project will be developed as a trainer for the game "Bioshock Infinite".

---
+++ trunk/BioShredderInfinite/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace BioShredderInfinite
+{
+	/// <summary>
+	/// Implements the logic behind the trainer's #MainWindow.
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		#region PUBLIC METHODS
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		#endregion
+	}
+}

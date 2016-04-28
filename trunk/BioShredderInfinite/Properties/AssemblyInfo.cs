3d0e661df674071873020584b780e4e960be6fb6
viniciusras <viniciusras@c4f62d13-1d80-41e3-aef9-1809c6e54439>
2016-04-28 15:32:39 +0000

    * Fixed GPL statement at the beggining of every file. The GPL statements on the beggining of every file were refering to the AquaRipper project (from which these statements have been copied), and not to the BioShredderInfinite project. * Added the GPL license to the "docs/License.rtf" file. This license is displayed when the user is installing the trainer. * Added the "BioShredderInfinite_SetupMSI" project for generating an MSI installer for the trainer. * Added the "BioShredderInfinite_SetupBundle" project for generating a setup for the trainer which is capable of installing both the trainer and the .NET Framework 4.5 (which is required in order for the trainer to run).

--- trunk/BioShredderInfinite/Properties/AssemblyInfo.cs
+++ trunk/BioShredderInfinite/Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
 ﻿/*
  * Copyright( C) 2014 Vinicius Rogério Araujo Silva
  *
- * This file is part of AquaRipper.
+ * This file is part of BioShredderInfinite.
  * 
- * AquaRipper is free software: you can redistribute it and/or modify
+ * BioShredderInfinite is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * AquaRipper is distributed in the hope that it will be useful,
+ * BioShredderInfinite is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with AquaRipper.If not, see<http://www.gnu.org/licenses/>.
+ * along with BioShredderInfinite.If not, see<http://www.gnu.org/licenses/>.
  */
 using System.Reflection;
 using System.Resources;
